99419e44c66cb4e7ab4d7de79d449cf59b46f564
camarello <camarello@student.dwc.edu>
2015-11-17 22:08:02 -0500

    PrimaryObjectiveUpdate
    Go from one recon point to the next

---
+++ Assets/Scripts/UpdatePrimaryObjective.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+// PRIMARY OBJECTIVE NARRATIVE //
+public class UpdatePrimaryObjective : MonoBehaviour 
+{
+
+	public GameObject PrimaryObjective2;
+	public GameObject PrimaryObjective3;
+	public AudioSource audio_objective_complete;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+
+	public void Awake()
+	{
+		audio_objective_complete = GetComponent<AudioSource> ();
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		audio_objective_complete.Play ();
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "primary_objective_waypoint_01") 
+		{
+			Destroy(col.gameObject);
+			PrimaryObjective2.SetActive (true);
+			audio_objective_complete.Play();
+
+		}
+
+		if (col.gameObject.name == "primary_objective_waypoint_02") 
+		{
+			Destroy(col.gameObject);
+			PrimaryObjective3.SetActive (true);
+			
+		}
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+}
