623241fd75c11db31453fa41381d2c5f2e9f2dde
Chrisl007 <lafond.chris@yahoo.com>
2015-11-18 13:02:44 -0500

    XML to UI
    read xml to ui

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections.Generic;
+using System.Xml;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public TextAsset Text_Asset;
+
+    void OnEnable()
+    {
+        if (Text_Asset == null) throw new System.Exception("Text_Asset not defined on Binder Script!");
+
+        XmlDocument xd = new XmlDocument();
+        xd.LoadXml(Text_Asset.text);
+
+        if (xd.DocumentElement.HasChildNodes)
+        {
+            Dictionary<string, Text> textComponents = new Dictionary<string, Text>(2);
+            Text[] t = gameObject.GetComponentsInChildren<Text>();
+
+            foreach (Text itm in t)
+                textComponents.Add(itm.name, itm);
+
+            foreach (XmlNode node in xd.DocumentElement.ChildNodes)
+            {
+                if (textComponents.ContainsKey(node.Name))
+                    textComponents[node.Name].text = node.InnerText;
+            }
+        }
+    }
+}
