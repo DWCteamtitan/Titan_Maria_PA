6061304dc0b55a37fa59bde7739a82ac2b4e5111
camarello <camarello@student.dwc.edu>
2015-11-18 20:14:59 -0500

    Added Large_Map
    Added Large_Map

--- Assets/Scripts/UpdatePrimaryObjective.cs
+++ Assets/Scripts/UpdatePrimaryObjective.cs
@@ -6,6 +6,7 @@ public class UpdatePrimaryObjective : MonoBehaviour
 
 	public GameObject PrimaryObjective2;
 	public GameObject PrimaryObjective3;
+	public GameObject text_recon_point_reached;
 
 	// Use this for initialization
 	void Start () 
@@ -24,18 +25,19 @@ public class UpdatePrimaryObjective : MonoBehaviour
 		{
 			Destroy(col.gameObject);
 			PrimaryObjective2.SetActive (true);
+			text_recon_point_reached.SetActive (true);
+
 		}
 
 		if (col.gameObject.name == "primary_objective_waypoint_02") 
 		{
 			Destroy(col.gameObject);
 			PrimaryObjective3.SetActive (true);
+			text_recon_point_reached.SetActive (true);
 			
 		}
 
 	}
-
-
 
 	// Update is called once per frame
 	void Update () 
